cb6e6e577a4257b4ea97479e72336f67a5de09b4
alinapugach <76130795+alinapugach@users.noreply.github.com>
2024-01-30 21:21:01 +0200

    Combined projects into one

---
+++ Project1/Program.cs
@@ -0,0 +1,27 @@
+using litak_back_end;
+using Microsoft.AspNetCore.Authentication;
+using MongoDB.Bson.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication("BasicAuthentication")
+    .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>("BasicAuthentication", null);
+BsonSerializer.RegisterSerializer(new StringObjectIdConverter());
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseCors(policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
