c3fde2812db22c0af0b825ee1d807381466357ef
Denis Karpenko <34599597+NapIl1@users.noreply.github.com>
2024-04-09 11:36:52 +0300

    updated toasts for user creation

--- Project1/ClientApp/src/app/pages/admin/components/users/users.component.ts
+++ Project1/ClientApp/src/app/pages/admin/components/users/users.component.ts
@@ -48,10 +48,19 @@ export class UsersComponent implements OnInit {
         }
       })
     } catch (error) {
-      this.toastService.showError(`Користувач з логіном ${this.login} вже існує.`);
+      if(error instanceof HttpErrorResponse){
+        if(error.status != 200){
+          console.log(error)
+          this.toastService.showError(`Користувач з позивним ${this.login} вже існує.`);
+        }
+      }
     }
 
     this.users = await this.userService.getAllUsers();
+    this.toastService.showSuccess(`Користувач з позивним ${this.login} був створений`);
+    this.login = '';
+    this.password = '';
+    this.role = UserRole.NOT_SELECTED;
   }
 
 }
